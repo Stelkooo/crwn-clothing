2e1af0c29ffe81cb485ba7fc439678ce8477b1a7
stelko <stilyanmtoskov@gmail.com>
2023-02-20 17:48:47 +0000

    user sign up mistype fix

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -28,7 +28,7 @@ export const signUpStart = (email, password, displayName) =>
   });
 
 export const signUpSuccess = (user, additionalDetails) =>
-  createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS, { user, additionalDetails });
+  createAction(USER_ACTION_TYPES.SIGN_UP_SUCCESS, { user, additionalDetails });
 
 export const signUpFailed = (error) =>
-  createAction(USER_ACTION_TYPES.SIGN_IN_FAILED, error);
+  createAction(USER_ACTION_TYPES.SIGN_UP_FAILED, error);
