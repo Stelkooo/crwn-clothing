b3d0b5376cdab518093c946a151acae22fd3cc93
stelko <stilyanmtoskov@gmail.com>
2023-02-20 21:22:23 +0000

    stripe setup

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 // import { compose, createStore, applyMiddleware } from 'redux';
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 // import { persistStore, persistReducer } from 'redux-persist';
 // import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
